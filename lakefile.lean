830da761bc4139f830c8264831ce499b970a2d40
Andreas Pfurtscheller <andreas@fimbler.local>
2024-05-25 01:00:48 +0200

    testing

--- lakefile.lean
+++ lakefile.lean
@@ -1,17 +1,24 @@
 import Lake
 open Lake DSL
 
+require batteries from git "https://github.com/leanprover-community/batteries" @ "main"
+
 package «evaluation» where
   -- add package configuration options here
-  srcDir := "src"
 
 lean_lib «Evaluation» where
   -- add library configuration options here
+  srcDir := "./src"
 
 lean_lib «Testing» where
-  -- add library configuration options here
-  srcDir := "../tests"
+  srcDir := "./util"
 
 @[default_target]
 lean_exe «evaluation» where
   root := `Main
+  srcDir := "./src"
+
+@[test_runner]
+lean_exe «tests» where
+  root := `Main
+  srcDir := "./tests"
