b172d5448948ee36b9413dee811c43df615dbec9
Andreas Pfurtscheller <1051396+aplr@users.noreply.github.com>
2024-05-25 03:34:33 +0200

    Testing (#1)
    * reusable workflows
    
    * testing
    
    * fix toolchain
    
    * fix docker build
    
    * lean tests
    
    ---------
    
    Co-authored-by: Andreas Pfurtscheller <andreas@fimbler.local>

--- lakefile.lean
+++ lakefile.lean
@@ -1,9 +1,24 @@
 import Lake
 open Lake DSL
 
-package «evaluation_function» where
+require batteries from git "https://github.com/leanprover-community/batteries" @ "main"
+
+package «evaluation» where
   -- add package configuration options here
 
+lean_lib «Evaluation» where
+  -- add library configuration options here
+  srcDir := "./src"
+
+lean_lib «Testing» where
+  srcDir := "./util"
+
 @[default_target]
-lean_exe «evaluation_function» where
+lean_exe «evaluation» where
   root := `Main
+  srcDir := "./src"
+
+@[test_runner]
+lean_exe «tests» where
+  root := `Main
+  srcDir := "./tests"
